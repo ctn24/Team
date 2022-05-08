5e0d68d71f418bcc95d7272484bc88a3df818598
Jorge_Palma <jp00831874@techmahindra.com>
2022-05-08 11:14:29 -0500

    default error correction in terraform variables.

--- deploy/terraform/variables.tf
+++ deploy/terraform/variables.tf
@@ -1,8 +1,8 @@
 # Azure GUIDS
-variable "subscription_id" {defdefault = "null"}
-variable "client_id" {defdefault = "null"}
-variable "client_secret" {defdefault = "null"}
-variable "tenant_id" {defdefault = "null"}
+variable "subscription_id" {default = null}
+variable "client_id" {default = null}
+variable "client_secret" {default = null}
+variable "tenant_id" {default = null}
 
 # Resource Group/Location
 variable "location" { default = "westus" }
@@ -20,5 +20,5 @@ variable "aks_name" { default = "aks-tf" }
 # VM - vault
 variable "vault_image_name" { default = "Vault-image" }
 variable "vm_size" { default = "D4ads_v5" }
-variable "admin_username" {defdefault = "null"}
-variable "admin_password" {defdefault = "null"}
+variable "admin_username" {default = null}
+variable "admin_password" {default = null}
