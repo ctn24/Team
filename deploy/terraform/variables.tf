3bfd489159450a9d494c99441ffb07b6131025e4
Jorge_Palma <jp00831874@techmahindra.com>
2022-05-08 18:31:23 -0500

    update pipeline terraform.

--- deploy/terraform/variables.tf
+++ deploy/terraform/variables.tf
@@ -10,16 +10,6 @@ variable "rg_name" { default = "rg-infra" }
 
 # ACR - Container Registry
 variable "acr_name" { default = "acr0tf" }
-variable "acr_password" {default = null}
-
-# Cosmosdb 
-variable "cosmos_name" { default = "cosmosdbaccount-tf" }
 
 # AKS
 variable "aks_name" { default = "aks-tf" }
-
-# VM - vault
-variable "vault_image_name" { default = "Vault-image" }
-variable "vm_size" { default = "D4ads_v5" }
-variable "admin_username" { default = null }
-variable "admin_password" { default = null }
