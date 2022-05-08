4af5df2801c7a44fd20e90e5516662b1a3234a08
Jorge_Palma <jp00831874@techmahindra.com>
2022-05-08 11:10:18 -0500

    update variables in to terraform.

--- deploy/terraform/variables.tf
+++ deploy/terraform/variables.tf
@@ -1,8 +1,8 @@
 # Azure GUIDS
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
+variable "subscription_id" {defdefault = null}
+variable "client_id" {defdefault = null}
+variable "client_secret" {defdefault = null}
+variable "tenant_id" {defdefault = null}
 
 # Resource Group/Location
 variable "location" { default = "westus" }
@@ -20,6 +20,5 @@ variable "aks_name" { default = "aks-tf" }
 # VM - vault
 variable "vault_image_name" { default = "Vault-image" }
 variable "vm_size" { default = "D4ads_v5" }
-variable "admin_username" {}
-variable "admin_password" {}
-
+variable "admin_username" {defdefault = null}
+variable "admin_password" {defdefault = null}
